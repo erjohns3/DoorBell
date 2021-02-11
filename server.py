b043e949860efd1c0c827c8e6e1df26dee5207b1
Eric Johnson <johnson.eric321@gmail.com>
2021-02-11 02:41:18 -0500

    threaded text to speech, youtube audio player, url chooser

--- server.py
+++ server.py
@@ -1,33 +1,82 @@
 import flask
-import tts
+import threading
+import gpiozero
 import pyttsx3
+import pafy
+import vlc
+
 
 app = flask.Flask(__name__)
-chime = "n/a"
 
-def speak(text):
-    print(text + " start")
+#################################### audio
+
+url = "null"
+Instance = vlc.Instance()
+player = Instance.media_player_new()
+
+def setURL(val):
+    global url
+    global Instance
+    global player
+
+    if val[0:30] == "https://www.youtube.com/watch?" :
+        url = val
+        video = pafy.new(url)
+        best = video.getbestaudio()
+        playurl = best.url
+        Media = Instance.media_new(playurl)
+        Media.get_mrl()
+        player.set_media(Media)
+
+
+setURL("https://www.youtube.com/watch?v=TO7z2FYB_mo")
+
+#################################### gpio signals
+
+def onPressed():
+    print("play")
+
+#button = gpiozero.Button(2)
+#button.when_pressed = onPressed
+
+#################################### text to speech
+
+def thread_speaker(text):
     engine = pyttsx3.init()
     engine.say(text)
     #engine.setProperty("rate", 1)
     engine.runAndWait()
-    print(text + " done")
 
+def speak(text):
+    x = threading.Thread(target=thread_speaker, args=(text,))
+    x.start()
 
-@app.route('/', methods = ['POST', 'GET'])
-def hello():
-    global chime
+################################## http server
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>', methods = ['POST', 'GET'])
+def catch_all(path): 
+    
+    global player
+    global url
+
     if flask.request.method == 'GET':
-        return flask.render_template('index.html', chime=chime)
+        return flask.render_template('index.html', url=url)
     elif flask.request.method == 'POST':
-        data = dict(flask.request.form)
         
-        if 'chime' in data.keys():
-            chime = data['chime']
-        if 'speech' in data.keys():
-            speak(data['speech'])
-        return flask.render_template('index.html', chime=chime)
+        if path == 'play':
+            player.play()
+        if path == 'stop':
+            player.stop()
+        if path == 'url' :
+            setURL(flask.request.form['url'])
+        if path == 'speech':
+            speak(flask.request.form['speech'])
+        
+        return flask.render_template('index.html', url=url)
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True, use_reloader=False)
+
+print("done")
