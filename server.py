8c6c2aecb3bc5d19fde9d6daffddbefdf50c7bcf
Eric Johnson <johnson.eric321@gmail.com>
2021-02-11 01:16:28 -0500

    text to speech

--- server.py
+++ server.py
@@ -1,23 +1,32 @@
-from flask import Flask, render_template, request
-app = Flask(__name__)
+import flask
+import tts
+import pyttsx3
 
+app = flask.Flask(__name__)
 chime = "n/a"
+
+def speak(text):
+    print(text + " start")
+    engine = pyttsx3.init()
+    engine.say(text)
+    #engine.setProperty("rate", 1)
+    engine.runAndWait()
+    print(text + " done")
+
 
 @app.route('/', methods = ['POST', 'GET'])
 def hello():
     global chime
-    if request.method == 'GET':
-        return render_template('index.html', chime=chime)
-    elif request.method == 'POST':
-        data = dict(request.form)
+    if flask.request.method == 'GET':
+        return flask.render_template('index.html', chime=chime)
+    elif flask.request.method == 'POST':
+        data = dict(flask.request.form)
         
         if 'chime' in data.keys():
             chime = data['chime']
-            print (data['chime'])
         if 'speech' in data.keys():
-            
-            print (data['speech'])
-        return render_template('index.html', chime=chime)
+            speak(data['speech'])
+        return flask.render_template('index.html', chime=chime)
 
 
 if __name__ == '__main__':
