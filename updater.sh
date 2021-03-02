55037677f2150485f9b6fc76687af37d187b6961
curtailing returns <gamechefproductions@gmail.com>
2021-03-02 18:22:38 -0500

    updater

--- updater.sh
+++ updater.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-    if [[ $(git --git-dir=/home/pi/programming/python/doorbell_getter/DoorBell/.git pull | grep 'Already up to date.' | wc -c ) -eq 0 ]]
+    if [[ $(git --git-dir=/home/pi/programming/python/doorbell_getter/DoorBell/.git --work-tree=/home/pi/programming/python/doorbell_getter/DoorBell/ pull | grep 'Already up to date.' | wc -c ) -eq 0 ]]
     then
         systemctl --user restart doorbell.service
         echo 'doorbell updated'
